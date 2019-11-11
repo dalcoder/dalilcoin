f5f5bd86b7327f908140460ec656dc0993e69170
Dal Coder <dalcoder@protonmail.com>
2019-11-11 20:03:47 +0000

    added broadcast_inv calls when valid txs and blocks are received

--- src/blocktree.ml
+++ src/blocktree.ml
@@ -335,6 +335,7 @@ let rec process_block sout validate forw dbp (lbh,ltxh) h ((bhd,bhs),bd) thtr th
       | Some(newtht,newsigt) ->
 	  Hashtbl.add validheadervals (lbh,ltxh) (bhd.tinfo,bhd.timestamp,bhd.newledgerroot,bhd.newtheoryroot,bhd.newsignaroot);
 	  Hashtbl.add validblockvals (lbh,ltxh) ();
+         broadcast_inv [(int_of_msgtype Headers,h);(int_of_msgtype Blockdelta,h)];
 	  sync_last_height := max !sync_last_height currhght;
 	  update_theories thtr tht newtht;
 	  update_signatures sgtr sgt newsigt;
@@ -1104,7 +1105,8 @@ Hashtbl.add msgtype_handler STx
 				    Hashtbl.add stxpool h stau;
 				    log_string (Printf.sprintf "Accepting tx %s into pool\n" (hashval_hexstring h));
 				    add_to_txpool h stau;
-				    savetxtopool_real h stau
+				    savetxtopool_real h stau;
+                                   broadcast_inv [(int_of_msgtype STx,h)];
 				  end
 				else
 				  (log_string (Printf.sprintf "ignoring tx %s with low fee of %s fraenks (%Ld cants)\n" (hashval_hexstring h) (Cryptocurr.fraenks_of_cants fee) fee))
